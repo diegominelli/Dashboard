13b5ff22840ba2fd00f553eda50761762053b66d
diegominelli <diego.minelli@hotmail.com>
2020-10-09 17:40:37 -0300

    Criando e inicializando o componente do dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule
+  ],
+  declarations: [
+    DashboardComponent
+  ],
+  exports: [
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
