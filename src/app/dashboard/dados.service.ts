fd80946345ddd5ff70a24cb1984a37c56119b8f3
diegominelli <diego.minelli@hotmail.com>
2020-10-13 17:07:53 -0300

    Implementando os graficos no componente

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DadosService {
 
   readonly dados = [  // readonly a variável não pode ser alterada //
